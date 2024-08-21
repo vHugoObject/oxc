71adcc2a6140edb843eee56d61942fd164f6da11
VictorHugoPilled <echase3@gmail.com>
2024-08-21 07:44:15 +0000

    fix(linter/react): Added is_import and import_matcher
    is_children now calls is_import in order to determine if Children
    comes from react.

--- crates/oxc_linter/src/rules/react/jsx_key.rs
+++ crates/oxc_linter/src/rules/react/jsx_key.rs
@@ -1,7 +1,7 @@
 use oxc_ast::{
     ast::{
-        CallExpression, Expression, JSXAttributeItem, JSXAttributeName, JSXElement, JSXFragment,
-        Statement,
+        Expression, JSXAttributeItem, JSXAttributeName, JSXElement, JSXFragment,
+        Statement, 
     },
     AstKind,
 };
@@ -72,9 +72,11 @@ impl Rule for JsxKey {
     }
 }
 
-pub fn is_to_array(call: &CallExpression) -> bool {
+pub fn is_to_array<'a, 'b>(node: &'b AstNode<'a>) -> bool {
     const TOARRAY: &str = "toArray";
-
+    
+    let AstKind::CallExpression(call) = node.kind() else { return false };
+    
     let Some(subject) = call.callee_name() else { return false };
 
     if subject != TOARRAY {
@@ -84,30 +86,64 @@ pub fn is_to_array(call: &CallExpression) -> bool {
     true
 }
 
-pub fn is_children(call: &CallExpression) -> bool {
+
+pub fn import_matcher<'a, 'b>(ctx: &'b LintContext<'a>, actual_local_name: String,
+    expected_module_name: &'a str) -> bool {
+
+        ctx.semantic().module_record().import_entries
+            .iter().any(|import| {
+                import.module_request.name().as_str() == expected_module_name.to_lowercase()
+                    && import.local_name.name().as_str() == actual_local_name.as_str()
+            })
+
+        
+
+}
+
+
+pub fn is_import<'a, 'b>(ctx: &'b LintContext<'a>, actual_local_name: &'a str,
+    expected_local_name: &'a str, expected_module_name: &'a str) -> bool {
+
+        let total_imports = ctx.semantic().module_record().requested_modules.len();
+        let total_variables = ctx.scopes().get_bindings(ctx.scopes().root_scope_id()).len();
+        
+        if total_variables == 0 && total_imports == 0 {            
+            return actual_local_name == expected_local_name
+        }
+      
+        return import_matcher(ctx, actual_local_name.to_string(), expected_module_name)       
+}
+
+
+
+pub fn is_children<'a, 'b>(node: &'b AstNode<'a>, ctx: &'b LintContext<'a>) -> bool {
+    const REACT: &str = "React";
     const CHILDREN: &str = "Children";
-
-    if let Some(member) = call.callee.as_member_expression() {
-        if let Expression::Identifier(ident) = member.object() {
-            return ident.name == CHILDREN;
-        }
-
-        if let Some(inner_member) = member.object().get_inner_expression().as_member_expression() {
-            return inner_member.static_property_name() == Some(CHILDREN);
-        }
+    
+    let AstKind::CallExpression(call) = node.kind() else { return false };
+    
+    let Some(member) = call.callee.as_member_expression() else { return false };
+    
+    
+    if let Expression::Identifier(ident) = member.object() {
+        return is_import(ctx, ident.name.as_str(), CHILDREN, REACT);
     }
+    
+    
+    let Some(inner_member) = member.object().get_inner_expression().as_member_expression() else { return false };
 
-    false
+    let Some(ident) = inner_member.object().get_identifier_reference() else { return false };
+    
+    let Some(local_name) = inner_member.static_property_name() else { return false };
+    
+    return is_import(ctx, ident.name.as_str(), REACT, REACT) && is_import(ctx, local_name, CHILDREN, REACT);
+
+    
 }
 fn is_within_children_to_array<'a, 'b>(node: &'b AstNode<'a>, ctx: &'b LintContext<'a>) -> bool {
-    let mut parents_iter = ctx.nodes().iter_parents(node.id()).skip(2);
-    parents_iter.any(|node| {
-        if let AstKind::CallExpression(expr) = node.kind() {
-            is_to_array(expr) && is_children(expr)
-        } else {
-            false
-        }
-    })
+    let parents_iter = ctx.nodes().iter_parents(node.id()).skip(2);
+    parents_iter.filter(|parent_node| matches!(parent_node.kind(), AstKind::CallExpression(_)))
+        .any(|parent_node| is_children(parent_node, ctx) && is_to_array(parent_node))                    
 }
 
 enum InsideArrayOrIterator {
@@ -454,18 +490,6 @@ fn test() {
         r#"import { Children } from "react";        
         Children.toArray([1, 2 ,3].map(x => <App />));
         "#,
-        r"
-        import Act from 'react';
-        import { Children as ReactChildren } from 'react';
-
-        const { Children } = Act;
-        const { toArray } = Children;
-
-        Act.Children.toArray([1, 2 ,3].map(x => <App />));
-        Act.Children.toArray(Array.from([1, 2 ,3], x => <App />));
-        Children.toArray([1, 2 ,3].map(x => <App />));
-        Children.toArray(Array.from([1, 2 ,3], x => <App />));
-        ",
         r"React.Children.toArray([1, 2 ,3].map(x => <App />));",
     ];
 
@@ -568,21 +592,7 @@ fn test() {
                 };
           ",
         r"foo.Children.toArray([1, 2 ,3].map(x => <App />));",
-        
-        r#"import { Children } from "react";
-        const Children = "foo";
-        Children.toArray([1, 2 ,3].map(x => <App />));
-        "#,
-
-        r#"import Act from 'react';
-        "const React = "foo";
-        React.Children.toArray([1, 2 ,3].map(x => <App />));
-        "#,
-
-        r#"const Children = "foo";
-        React.Children.toArray([1, 2 ,3].map(x => <App />));
-        "#,
-  
+    
     ];
 
     Tester::new(JsxKey::NAME, pass, fail).test_and_snapshot();
